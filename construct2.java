ba4f46ce265c1e616ac20a6900b77be62b95c2cf
nitin110 <56267313+nitin110@users.noreply.github.com>
2022-12-08 15:50:02 +0530

    Add files via upload

---
+++ construct2.java
@@ -0,0 +1,19 @@
+package com.mufg;
+
+public class construct2 {
+int id;
+String name;
+
+void display() {
+	System.out.println(id +" "+name);
+}
+	public static void main(String[] args) {
+
+construct2 cs=new construct2();
+construct2 cb=new construct2();
+
+cs.display();
+cb.display();
+	}
+
+}
