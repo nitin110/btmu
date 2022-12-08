a56c9fcbc500ca69a603fc09369e535db7d4b040
nitin110 <56267313+nitin110@users.noreply.github.com>
2022-12-08 15:47:29 +0530

    Create program1.java

---
+++ program1.java
@@ -0,0 +1,12 @@
+public class construct {
+
+	construct() {
+		System.out.println("bike is manufactured");
+	}
+	
+	public static void main(String[] args) {
+		construct b=new construct();
+		
+	}
+
+}
